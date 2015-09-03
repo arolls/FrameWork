0ff544e0826e2b383bd622068dda6f7d41240986
Alex Rolls <arolls99@gmail.com>
2015-09-03 18:51:48 -0400

    Master Commit

---
+++ src/main/java/mycompany/Library.java
@@ -0,0 +1,20 @@
+package mycompany;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Library {
+
+  public static void headerCheck(final WebDriver driver) {
+    driver.findElement(By.linkText("Standards")).click();
+    driver.findElement(By.linkText("Participate")).click();
+    driver.findElement(By.linkText("Membership")).click();
+    driver.findElement(By.linkText("About W3C")).click();
+  }
+
+  public static void participateCheck(final WebDriver driver) {
+    driver.findElement(By.linkText("Participate")).click();
+  }
+}
+
+
