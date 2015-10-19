58dd864420513811aa89d7fcab74ac3e0a820b54
Alex Rolls <arolls@inviqa.com>
2015-10-19 15:07:18 +0100

    POM update and imports

--- src/main/java/mycompany/Library.java
+++ src/main/java/mycompany/Library.java
@@ -2,6 +2,12 @@ package mycompany;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.concurrent.TimeUnit;
+import static org.testng.Assert.*;
 
 public class Library {
 
